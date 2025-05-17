560251a0a3f2c8a6ec4da36e106d70a2d42d1fe9
firomsa <firomsassf@gmail.com>
2025-05-17 23:35:02 +0300

    feat - added responsiveness

--- travel-app/src/components/MainContent.jsx
+++ travel-app/src/components/MainContent.jsx
@@ -3,11 +3,12 @@ import travelItems from "../api/data"
 
 function MainContent() {
   return (
-    <div className="mx-40 flex flex-col gap-10 my-15">
+    <div className="mx-5 sm:mx-10 md:mx-40 flex flex-col gap-10 my-15">
         {
             travelItems.map((a) => <TravelItem Travel={a}/>)
         }
     </div>
   )
 }
+
 export default MainContent
--- travel-app/src/components/TravelItem.jsx
+++ travel-app/src/components/TravelItem.jsx
@@ -2,8 +2,8 @@ import location from "../assets/images/location.svg";
 
 function TravelItem({ Travel }) {
     return (
-        <div className="flex gap-8 items-center">
-            <div className="h-88 w-64">
+        <div className="flex flex-col md:flex-row gap-8 items-center">
+            <div className="h-88 w-auto md:w-64">
                 <img className="w-full h-full rounded-md" src={Travel.img.src} alt={Travel.img.alt} />
             </div>
             <div className="flex-3/4">
@@ -13,7 +13,7 @@ function TravelItem({ Travel }) {
                     <a className="text-grayish font-light underline underline-offset-4" href={Travel.googleMapsLink}>View on Google Maps</a>
                 </div>
 
-                <p className="font-bold text-6xl pb-8">{Travel.title}</p>
+                <p className="font-bold text-3xl sm:text-6xl pb-8">{Travel.title}</p>
                 <p className="font-bold pb-3">{Travel.dates}</p>
                 <p className="">{Travel.text}</p>
             </div>
