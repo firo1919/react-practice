42b194b26310b45630874bb3ce86fbdb52024cce
firomsa <firomsassf@gmail.com>
2025-05-18 03:18:20 +0300

    fix - added a key prop to array mapping

--- travel-app/src/components/MainContent.jsx
+++ travel-app/src/components/MainContent.jsx
@@ -5,7 +5,7 @@ function MainContent() {
   return (
     <div className="mx-5 sm:mx-10 md:mx-40 flex flex-col gap-10 my-15">
         {
-            travelItems.map((a) => <TravelItem Travel={a}/>)
+            travelItems.map((a) => <TravelItem key={a.id} Travel={a}/>)
         }
     </div>
   )
