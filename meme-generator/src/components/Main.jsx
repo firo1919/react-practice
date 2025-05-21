1362bf55cb2536ec3fd2d1e40a360340bc0aad73
firomsa <firomsassf@gmail.com>
2025-05-21 16:19:21 +0300

    feat - added random image fetching

--- meme-generator/src/components/Main.jsx
+++ meme-generator/src/components/Main.jsx
@@ -1,11 +1,24 @@
+import { useEffect } from "react";
 import { useState } from "react";
 
 function Main() {
 	const [memeInfo, setMemeInfo] = useState({
 		topText: "Shut Up",
 		bottomText: "and take it",
-		image: "https://placehold.co/600x400",
+		image: "http://i.imgflip.com/1bij.jpg",
 	});
+
+    const [allMemes, setAllMemes] = useState([])
+
+    useEffect(() => {
+        fetch("https://corsproxy.io/?url=https://api.imgflip.com/get_memes/")
+            .then((response) => response.json())
+            .then((data) => {
+                setAllMemes(data.data.memes)
+                console.log(data.data.memes)
+            })
+    }, [])
+    
 	function handleSetMemeInfo(event) {
 		const id = event.currentTarget.name;
 		const text = event.currentTarget.value;
@@ -21,6 +34,17 @@ function Main() {
 			}));
 		}
 	}
+
+    function handleSetMemeImage(){
+        let idx = Math.floor(Math.random() * (100))
+        const newImage = allMemes[idx].url
+        console.log(newImage)
+        setMemeInfo((prev) => ({
+            ...prev,
+            image:newImage
+        }))
+    }
+
 	return (
 		<div className=" w-3xl mx-auto py-10 grid grid-cols-2 gap-8">
 			<div className="flex flex-col gap-2">
@@ -31,9 +55,9 @@ function Main() {
 				<label htmlFor="bottom">Bottom Text</label>
 				<input onChange={handleSetMemeInfo} className="border border-gray-500 p-2 rounded-md " placeholder={memeInfo.bottomText} type="text" id="bottom" name="bottom" />
 			</div>
-			<button className="col-span-2 bg-linear-to-r from-[#672280] to-[#A626D3] h-10 rounded-md font-bold text-lg text-white">Get a new meme image 🖼️</button>
+			<button onClick={handleSetMemeImage} className="col-span-2 bg-linear-to-r from-[#672280] to-[#A626D3] h-10 rounded-md font-bold text-lg text-white">Get a new meme image 🖼️</button>
 			<div className="max-h-100 col-span-2 relative">
-				<img className="h-full w-full object-cover" src="https://picsum.photos/200/300" />
+				<img className="h-full w-full object-cover" src={memeInfo.image} />
 				<span className="absolute top-0 w-full text-center font-anton text-5xl uppercase pt-10 text-white">{memeInfo.topText}</span>
 				<span className="absolute bottom-0 w-full text-center font-anton text-5xl uppercase pb-10 text-white">{memeInfo.bottomText}</span>
 			</div>
