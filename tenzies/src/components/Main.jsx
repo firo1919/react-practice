ab2abc3a90178a448aa542c95301eae93b7f5df4
firomsa <firomsassf@gmail.com>
2025-05-23 15:26:39 +0300

    feat - added game won cofetti, new game ...

--- tenzies/src/components/Main.jsx
+++ tenzies/src/components/Main.jsx
@@ -1,9 +1,27 @@
 import Die from "./Die";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { nanoid } from "nanoid";
+import Confetti from 'react-confetti'
+
 
 function Main() {
-	const [dices, setDices] = useState([]);
+	const [dices, setDices] = useState(() => allNewDice());
+    const gamewon = gameWon()
+    const buttonRef = useRef(null)
+
+	function gameWon() {
+		if (dices.length !== 0) {
+            let prev = dices[0].value
+			for (let i = 0; i < dices.length; i += 1) {
+				if (dices[i].isHeld === false | dices[i].value !== prev) {
+					return false;
+				}
+			}
+
+			return true;
+		}
+        return false
+	}
 
 	function allNewDice() {
 		let ans = [];
@@ -17,26 +35,29 @@ function Main() {
 	}
 
 	function hold(id) {
-        setDices((oldDices => {
-            return oldDices.map(die => {
-                return die.id == id ? {...die, isHeld:!die.isHeld}:die
-            })
-        }))
-            
+		setDices((oldDices) => {
+			return oldDices.map((die) => {
+				return die.id == id ? { ...die, isHeld: !die.isHeld } : die;
+			});
+		});
 	}
 	function rollDices() {
-		setDices(oldDices =>{
-            return oldDices.map(die => {
-                return !die.isHeld ? {...die, value:Math.floor(Math.random() * 6 + 1)} : die
-            })
-        });
+		setDices((oldDices) => {
+			return oldDices.map((die) => {
+				return !die.isHeld ? { ...die, value: Math.floor(Math.random() * 6 + 1) } : die;
+			});
+		});
 	}
-	useEffect(() => {
-		setDices(allNewDice);
-	}, []);
-	console.log(dices);
+
+    useEffect(() => {
+        if(gamewon){
+            buttonRef.current.focus()
+        }
+    }, [gamewon])
+
 	return (
 		<div className="w-dvw h-dvh flex items-center justify-center">
+            {gamewon && <Confetti />}
 			<div className="w-90 h-96 bg-[#0B2434] relative">
 				<div className="absolute top-6 bottom-6 right-5 left-5 rounded-xl bg-[#F5F5F5] flex flex-col items-center py-8 px-4 gap-4">
 					<h1 className="font-bold text-2xl">Tenzies</h1>
@@ -46,8 +67,8 @@ function Main() {
 							<Die key={a.id} die={a} hold={hold} />
 						))}
 					</div>
-					<button className="bg-blue-700 rounded-sm text-white font-bold w-30 h-20 hover:bg-blue-800 active:bg-blue-600" onClick={rollDices}>
-						Roll
+					<button ref={buttonRef} className="bg-blue-700 rounded-sm text-white font-bold w-30 h-20 hover:bg-blue-800 active:bg-blue-600 focus:border-blue-300 focus:border-4" onClick={gamewon ? () =>{setDices(allNewDice)} : rollDices}>
+						{!gamewon ? "Roll" : "New game"}
 					</button>
 				</div>
 			</div>
