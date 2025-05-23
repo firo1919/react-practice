fba5d4c88c5c01599b97182160ef2c169fc71121
firomsa <firomsassf@gmail.com>
2025-05-24 00:45:26 +0300

    fix - fairwell centered

--- assembly-endgame/src/components/FareWell.jsx
+++ assembly-endgame/src/components/FareWell.jsx
@@ -1,7 +1,7 @@
 function FareWell({ Languages }) {
 	const removedLetters = Languages.filter((a) => a.removed).map((a) => a.name);
 	if (removedLetters.length !== 0) {
-		return <div className="h-17 mt-4 bg-purple-500 text-white italic flex justify-center items-center rounded-sm text-lg">"Farewell {removedLetters.join(" & ")}"</div>;
+		return <div className="h-17 mt-4 bg-purple-500 text-white italic flex justify-center items-center rounded-sm text-lg text-center">"Farewell {removedLetters.join(" & ")}"</div>;
 	}
 	return <div className="h-17 mt-4 text-white italic"></div>;
 }
