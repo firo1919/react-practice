6fedae90f80b7b71f83a0bfb990567a964d58874
firomsa <firomsassf@gmail.com>
2025-05-29 11:13:25 +0300

    fix - changed to object definition for classes

--- component-library/src/App.jsx
+++ component-library/src/App.jsx
@@ -2,7 +2,7 @@ import Badge from "./components/Badge/Badge";
 
 function App() {
 	return (
-        <Badge variant="square" color="gray">Badge</Badge>
+        <Badge variant="pill" color="pink">Badge</Badge>
     );
 }
 export default App;
--- component-library/src/components/Badge/Badge.jsx
+++ component-library/src/components/Badge/Badge.jsx
@@ -1,18 +1,23 @@
-function Badge({variant, color, children}) {
-    /* all supported classes for color props 
+function Badge({variant="square", color="gray", children}) {
 
-    bg-gray-100 text-gray-950
-    bg-yellow-100 text-yellow-950
-    bg-red-100 text-red-950
-    bg-green-100 text-green-950
-    bg-blue-100 text-blue-950
-    bg-indigo-100 text-indigo-950
-    bg-purple-100 text-purple-950
-    bg-pink-100 text-pink-950
+    const colors = {
+        gray : "bg-gray-100 text-gray-950",
+        yellow : "bg-yellow-100 text-yellow-950",
+        red : "bg-red-100 text-red-950",
+        green : "bg-green-100 text-green-950",
+        blue : "bg-blue-100 text-blue-950",
+        indigo : "bg-indigo-100 text-indigo-950",
+        purple : "bg-purple-100 text-purple-950",
+        pink : "bg-pink-100 text-pink-950",
+    }
 
-    */
+    const variants = {
+        square : "rounded-sm",
+        pill : "rounded-full"
+    }
+    
 	return (
-        <div className={`m-2 w-17 h-6 px-3 py-0.5 flex items-center justify-center bg-${color}-100 text-${color}-950 ${variant=="square" ? "rounded-sm":"rounded-full"} font-medium text-sm`}>{children}</div>
+        <div className={`m-2 w-17 h-6 px-3 py-0.5 flex items-center justify-center ${colors[color]} ${variants[variant]} font-medium text-sm`}>{children}</div>
     );
 }
 export default Badge;
